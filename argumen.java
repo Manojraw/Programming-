20aeabd106ae344f6c64f96eb2610f78dc59ec19
manoj meena <manojmeena4558@gmail.com>
2023-11-15 17:11:25 +0530

    argu

--- argumen.java
+++ argumen.java
@@ -1,5 +1,7 @@
 public class argumen {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
-    }
+public static void main(String[] args) {
+     System.out.println(args[0]);
+
+}
+
 }
